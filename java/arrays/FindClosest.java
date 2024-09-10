480fe2532b20b812e5f779ef59c46afd5e44187b
Sharad Mishra <sharadm2001@gmail.com>
2024-09-10 16:36:04 +0530

    "arrays problems"

---
+++ java/arrays/FindClosest.java
@@ -0,0 +1,16 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class FindClosest {
+    public int findClosestNumber(int[] nums) {
+        int ans = 0;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(Math.abs(nums[i]) < Math.abs(nums[ans])) {
+                ans = i;
+            }
+        }
+        return nums[ans];
+    }
+}
