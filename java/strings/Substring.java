8594a3d58ec6da712b6e5fdac863382d31735220
Sharad Mishra <sharadm2001@gmail.com>
2024-09-22 00:32:28 +0530

    "substring double pointer"

--- java/strings/Substring.java
+++ java/strings/Substring.java
@@ -5,24 +5,20 @@ import java.util.*;
 // two pointer, dynamic programming
 public class Substring {
     public static void main(String[] args) {
-      String s1 = "abc", t1 = "ahbgdc",s = "axc", t = "ahbgdc";
+      String s1 = "ahb", t1 = "ahbgdc",s = "axc", t = "ahbgdc";
       System.out.println(isSubsequence(s, t) + " ," + isSubsequence(s1, t1));
 
     }
    
     public static boolean isSubsequence(String s, String t) {
-        LinkedList<Character> stack = new LinkedList<>();
-        int i=0,j=0;
-        for(i=0;i<s.length();i++){
-            stack.add(i,s.charAt(i));
+        if (s.length() ==0) return true;
+        char[] c = s.toCharArray();
+        int sp = 0;
+        for( char tc : t.toCharArray()){
+            if(c[sp] == tc) sp++;
+            if(sp == s.length()) return true;
         }
-        
-        for(j=0;j<t.length();j++){
-            if(stack.contains(t.charAt(j))) stack.remove(stack.indexOf(t.charAt(j)));
-        }
-        
-        if(stack.size()==0) return true;
-        return false;
+        return sp==s.length();
     }
 
 }
